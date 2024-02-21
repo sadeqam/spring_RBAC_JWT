49b36352aa27fe150ee45db0f352e84dff91fb78
sadeq <rexaei.sadeqam@gmail.com>
2024-02-21 09:37:00 +0330

    Initial commit

---
+++ src/main/java/ir/sadeqam/spring_RBAC_JWT/SpringRbacJwtApplication.java
@@ -0,0 +1,13 @@
+package ir.sadeqam.spring_RBAC_JWT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRbacJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRbacJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ir/sadeqam/spring_RBAC_JWT/SpringRbacJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.sadeqam.spring_RBAC_JWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRbacJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
